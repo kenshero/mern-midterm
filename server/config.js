b2f6e92faf640b1a95edd6a972ffc2dbdf86acc8
kenshero <kenshero.k@gmail.com>
2016-03-19 22:05:10 +0700

    aa

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://54.254.235.238:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
